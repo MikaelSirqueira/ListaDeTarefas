a0eb6f3524c55ef2ea2b3644c9e2f102e5b9bb7e
Mikael Sirqueira <silmike14@yahoo.com.br>
2023-10-31 01:03:01 -0300

    -add: integração entre back-end e front-end; -feat: ajuste nas fucionalidade da tela; -feat: dados sendo gravados usando sqlite

--- front/pro-atividade-app/src/App.jsx
+++ front/pro-atividade-app/src/App.jsx
@@ -1,45 +1,51 @@
 import { useState, useEffect } from "react";
-import "./App.css";
 import AtividadeForm from "./components/AtividadeForm";
 import AtividadeLista from "./components/AtividadeLista";
+import api from "./api/atividade";
 
 function App() {
-  const [index, setIndex] = useState(0);
   const [atividades, setAtividades] = useState([]);
   const [atividade, setAtividade] = useState({ id: 0 });
 
-  useEffect(() => {
-    atividades.length <= 0
-      ? setIndex(1)
-      : setIndex(
-          Math.max.apply(
-            Math,
-            atividades.map((item) => item.id)
-          ) + 1
-        );
-  }, [atividades]);
+  const retornaTodasAtividades = async () => {
+    const response = await api.get("atividade");
+    return response.data;
+  };
 
-  function addAtividade(ativ) {
-    setAtividades([...atividades, { ...ativ, id: index }]);
-  }
+  useEffect(() => {
+    const getAtividades = async () => {
+      const todasAtividades = await retornaTodasAtividades();
+      if (todasAtividades) setAtividades(todasAtividades);
+    };
+    getAtividades();
+  }, []);
+
+  const addAtividade = async (ativ) => {
+    const response = await api.post("atividade", ativ);
+    setAtividades([...atividades, response.data]);
+  };
 
   function cancelarAtividade() {
     setAtividade({ id: 0 });
   }
 
-  function atualizarAtividade(ativ) {
+  const atualizarAtividade = async (ativ) => {
+    const response = await api.put(`atividade/${ativ.id}`, ativ);
+    const { id } = response.data;
     setAtividades(
-      atividades.map((item) => (item.id === ativ.id ? ativ : item))
+      atividades.map((item) => (item.id === id ? response.data : item))
     );
     setAtividade({ id: 0 });
   }
 
-  function deletarAtividade(id) {
-    const atividadesFiltradas = atividades.filter(
-      (atividade) => atividade.id !== id
-    );
-    setAtividades([...atividadesFiltradas]);
-  }
+  const deletarAtividade = async (id) => {
+    if (await api.delete(`atividade/${id}`)) {
+      const atividadesFiltradas = atividades.filter(
+        (atividade) => atividade.id !== id
+      );
+      setAtividades([...atividadesFiltradas]);
+    }
+  };
 
   function editarAtividade(id) {
     const atividade = atividades.filter((atividade) => atividade.id === id);
--- front/pro-atividade-app/src/components/Atividade.jsx
+++ front/pro-atividade-app/src/components/Atividade.jsx
@@ -3,12 +3,9 @@ import React from "react";
 export default function Atividades(props) {
   function prioridadeLabel(param) {
     switch (param) {
-      case "1":
-        return "Baixa";
-      case "2":
-        return "Normal";
-      case "3":
-        return "Alta";
+      case 'Baixa':
+      case 'Normal':
+      case 'Alta':
       default:
         return "Não definido";
     }
@@ -16,11 +13,11 @@ export default function Atividades(props) {
 
   function prioridadeStyle(param, icon) {
     switch (param) {
-      case "1":
+      case  "Baixa":
         return icon ? "fa-smile" : "success";
-      case "2":
+      case  "Normal":
         return icon ? "fa-meh" : "warning";
-      case "3":
+      case "Alta":
         return icon ? "fa-frown" : "danger";
       default:
         return "fa-face-meh-blank";
